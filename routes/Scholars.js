eed66c99baa1b0f5632114d5ef41bd51918c5832
itsArjae <arjaeiporong@gmail.com>
2022-10-23 19:51:03 +0800

    distinct year

--- routes/Scholars.js
+++ routes/Scholars.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Scholarsrecords } = require('../models');
-
+var sequelize = require('sequelize');
 const scholarsController = require('../controller/scholars/scholars.controller');
 router.post('/create',scholarsController.saveScholars);
 router.get('/fetch/batch/:id/:bid',scholarsController.getScholarsBatch);
@@ -22,4 +22,38 @@ router.get("/count", async (req, res, next) => {
     
   });
 //router.post('/update',);
+
+
+router.get('/distinct/year',async(req,res,next)=>{
+
+   const findYear = await Scholarsrecords.findAll({
+    attributes: ['year']
+});
+
+let newSet = [];
+
+findYear.map((data)=>{
+    newSet.push(data.year)
+})
+
+
+let uniqueChars = [];
+newSet.forEach((element) => {
+    if (!uniqueChars.includes(element)) {
+        uniqueChars.push(element);
+    }
+});
+
+let countYear = [];
+
+    for(var i=0; i<uniqueChars.length; i++){
+         const addData = await Scholarsrecords.count({
+            where:{
+                year:uniqueChars[i]
+            }
+         })
+        countYear.push({year:uniqueChars[i],value:addData});
+    }
+  res.json(countYear)
+})
 module.exports = router;
