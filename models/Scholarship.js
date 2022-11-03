9b254099ebfaeb6e5dd493ce6414f0746f11c6e6
itsAllyx <allyzzaguanzing@gmail.com>
2022-11-03 22:58:22 +0800

    backend

--- models/Scholarship.js
+++ models/Scholarship.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    
   });
 
   Scholarships.associate = (models) => {
--- routes/Scholarship.js
+++ routes/Scholarship.js
@@ -16,6 +16,72 @@ router.get("/fetch/count", async (req, res, next) => {
   res.json(size);
 });
 
+router.get("/count/cat", async (req, res, next) => {
+  
+  const year = new Date();
+  let governorSize = 0;
+  let congSize = 0;
+  let mayorSize = 0;
+  let skSize = 0;
+  let finAssSize = 0;
+  let govSize = 0;
+  let ngoSize = 0;
+  let othersSize = 0;
+
+    const active = await Scholarsrecords.findAll({
+      year:year.getFullYear()
+    });
+    let data = [];
+    const scholarship = await Scholarship.findAll();
+
+    for(var i = 0; i<scholarship.length; i++){
+      let size = await Scholarsrecords.count({
+        where:{ScholarshipId:scholarship[i].id,
+        year:year.getFullYear()
+        }
+      });
+
+      if(scholarship[i].category == "Governor"){
+        governorSize = governorSize + size;
+      }
+      if(scholarship[i].category == "Congressman"){
+        congSize = congSize + size;
+      }
+      if(scholarship[i].category == "Mayor"){
+        mayorSize = mayorSize + size;
+      }
+      if(scholarship[i].category == "SK"){
+        skSize = skSize + size;
+      }
+      if(scholarship[i].category == "Financial Assistance"){
+        finAssSize = finAssSize + size;
+      }
+      if(scholarship[i].category == "Government"){
+        govSize = govSize + size;
+      }
+      if(scholarship[i].category == "NGO"){
+        ngoSize = ngoSize + size;
+      }
+      if(scholarship[i].category == "Others"){
+        othersSize = othersSize + size;
+      }
+
+    }
+
+
+  res.json([
+  {name:"Governor",value:governorSize},
+  {name:"Congressman",value:congSize},
+  {name:"Mayor",value:mayorSize},
+  {name:"SK",value:skSize},
+  {name:"Financial Assistance",value:finAssSize},
+  {name:"Government",value:govSize},
+  {name:"NGO",value:ngoSize},
+  {name:"Others",value:othersSize}
+])
+  
+});
+
 router.post("/update/duplication/:id", async (req, res, next) => {
   const id = req.params.id;
   const data = req.body;
