6c820500ebb201ee7cb99a8d5b128f4a54676c48
itsAllyx <allyzzaguanzing@gmail.com>
2022-12-03 21:45:00 +0800

    add

--- models/Scholarsrecord.js
+++ models/Scholarsrecord.js
@@ -44,6 +44,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 
   
